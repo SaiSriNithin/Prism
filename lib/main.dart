c9f5dd20f2854f8a88bd0988517ad0cbb818c078
nikhil-1613 <nikhilgupta1613@gmail.com>
2024-05-11 12:01:09 +0530

    modified:   lib/main.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml
    Untracked files:
    	assets/images/
    	lib/events_screen.dart
    	lib/screens/community_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:new_prism/navbar.dart';
-import 'package:new_prism/screens/splash_and_login_screens/onboarding_scrrens/onboarding_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-var greenColor = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 17, 79, 90),
-);
-var goldColor = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 251, 171, 58),
-);
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final prefs = await SharedPreferences.getInstance();
-  bool onboarding = prefs.getBool("onboarding") ?? false;
-  runApp(MyApp(
-    onboarding: onboarding,
-  ));
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -36,41 +20,96 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
-        colorScheme: const ColorScheme(
-          primary: Color.fromARGB(255, 17, 79, 90),
-          onPrimary: Color.fromARGB(150, 17, 79, 90),
-          secondary: Color.fromARGB(255, 251, 171, 58),
-          onSecondary: Color.fromARGB(150, 251, 171, 58),
-          background: Color.fromARGB(255, 247, 248, 251),
-          onBackground: Color.fromARGB(110, 247, 248, 251),
-          error: Colors.red,
-          onError: Colors.red,
-          surface: Color.fromARGB(255, 255, 255, 255),
-          onSurface: Color.fromARGB(255, 255, 255, 255),
-          brightness: Brightness.light,
-        ),
-        textTheme: TextTheme(
-          displayLarge: const TextStyle(
-            fontSize: 72,
-            fontWeight: FontWeight.bold,
-          ),
-          titleLarge: GoogleFonts.oswald(
-            fontSize: 30,
-            fontStyle: FontStyle.italic,
-          ),
-          bodyMedium: GoogleFonts.merriweather(
-            fontSize: 35,
-            fontWeight: FontWeight.bold,
-          ),
-          displaySmall: GoogleFonts.pacifico(),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
       ),
-
-      //this is test tharak
-      // home: Homescreen(),
-      home: widget.onboarding ? const Navbar1() : const OnboardingScreen(),
-      //home: Community(),
-      // go to hell
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
